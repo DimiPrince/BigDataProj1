6b4b824d35820e892845c63fc342f531f4920e3b
Dimitry Principale <dimitryprincipale@gmail.com>
2024-03-30 01:32:00 -0400

    uploaded data to mongo

--- BDproject1.py
+++ BDproject1.py
@@ -25,6 +25,12 @@ with open('city_inspections.json') as f:
         # Replace the '_id' field with the value of the '$oid' field
         document['_id'] = document['_id']['$oid']
         data.append(document)
+'''
+was getting error for "_id fields may not contain '$'-prefixed fields: $oid is not valid for storage."
+To fix this issue, I had to change script  
+to remove the $oid field from the _id field of
+ each document before inserting it into the database. 
+''' 
 
 # Insert the data into the 'city_inspections' collection
 db.city_inspections.insert_many(data)
