dae7d4bbd26393ea602aaf9d834a8bc3fe48ec0e
Dimitry Principale <dimitryprincipale@gmail.com>
2024-03-30 11:18:19 -0400

    uploaded data to mongo and sample file for testing

--- setupTest.py
+++ setupTest.py
@@ -5,6 +5,7 @@
 #can test if you are being connected to the database by using the following code
 
 import pymongo
+import certifi
 from urllib.parse import quote_plus
 
 username = quote_plus("dprincipale")#change this y1our username
@@ -13,10 +14,13 @@ cluster = "cluster0.hnstfom"
 
 uri = f"mongodb+srv://{username}:{password}@{cluster}.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-client = pymongo.MongoClient(uri)
+client = pymongo.MongoClient(uri, tlsCAFile=certifi.where())
+'''have to add second parameter of certificate, kept failing to establish connection without it
+connection without it , "pip install certifi" in directory where your
+python  is installed'''
 
-db = client["sample_mflix"]
-collection = db["comments"]
+db = client["BigData"]
+collection = db["city_inspections"]
 
 for document in collection.find():
     print(document)
